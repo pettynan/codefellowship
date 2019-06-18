ac7e4dfd26969709bb95d6733435b117ab21e07b
Peter Tynan <pettynan@amazon.com>
2019-06-18 13:09:51 -0700

    work in progress

--- src/main/java/com/tynan/codefellowship/codefellowship/CodeFellowshipController.java
+++ src/main/java/com/tynan/codefellowship/codefellowship/CodeFellowshipController.java
@@ -1,15 +1,20 @@
 package com.tynan.codefellowship.codefellowship;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.security.Principal;
+
+@Controller
 public class CodeFellowshipController {
 
     @GetMapping("/codefellowship")
-    public String getCodefellowship() {
-        return "they're taking the hobbits to isengaard";
+    public String getCodefellowship(Principal p, Model m) {
+        System.out.println(p.getName());
+        m.addAttribute("principal", p);
+        return "codefellowship";
     }
 
 
